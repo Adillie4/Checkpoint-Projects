9dbde185f404973cd3928a9486ca2236b99deb4a
Adillie4 <73258065+Adillie4@users.noreply.github.com>
2020-11-15 16:08:35 -0600

    Create employeeManA.js

---
+++ employeeManA.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {ec:'@bO',oGn:'Bernice Ortiz',oGp:'4824931093',oGe:'bortiz@gmail.com',oGt:'CEO',nE:'Bernice Ort',pNe:'8675309999',eE:'edit1@gmail.com',tE:'t1'},
+    {ec:'@mB',oGn:'Marnie Barnett',oGp:'3094812387',oGe:'mbarnett@hotmail.com',oGt:'CTO',nE:'Marnie Bar',pNe:'8675309998',eE:'edit2@gmail.com',tE:'t2'},
+    {ec:'@pW',oGn:'Phillip Weaver',oGp:'7459831843',oGe:'theWeavester@gmail.com',oGt:'Manager',nE:'Phil',pNe:'8675309997',eE:'edit3@gmail.com',tE:'t3'},
+    {ec:'@tO',oGn:'Teresa Osborne',oGp:'3841238745',oGe:'Terborne@yahoo.com',oGt:'Director of Engineering',nE:'Tessa',pNe:'8675309996',eE:'edit4@gmail.com',tE:'t4'},
+    {ec:'@dB',oGn:'Dollie Berry',oGp:'4873459812',oGe:'DollBerry@yahoo.com',oGt:'Front-End Developer',nE:'Dolly',pNe:'8675309995',eE:'edit5@gmail.com',tE:'t5'},
+    {ec:'@hW',oGn:'Harriett Williamson',oGp:'6571249801',oGe:'HmicWilliam@gmail.com',oGt:'Front-End Developer',nE:'Harry',pNe:'8675309994',eE:'edit6@gmail.com',tE:'t6'},
+    {ec:'@rE',oGn:'Ruby Estrada',oGp:'5740923478',oGe:'SaphireEstrada@hotmail.com',oGt:'Back-End Developer',nE:'Ruby E',pNe:'8675309993',eE:'edit7@gmail.com',tE:'t7'},
+    {ec:'@lW',oGn:'Lou White',oGp:'8727813498',oGe:'LouLou@yahoo.com',oGt:'Full-Stack Developer',nE:'Lou',pNe:'8675309992',eE:'edit8@gmail.com',tE:'t8'},
+    {ec:'@lB',oGn:'Lois Brewer',oGp:'8749823456',oGe:'ElBrewsky@gmail.com',oGt:'Sales Manager',nE:'Lois',pNe:'8675309991',eE:'edit9@gmail.com',tE:'t9'},
+    {ec:'@Sti',oGn:'Stitch',oGp:'1800123456',oGe:'alohagalaxy@gmail.com',oGt:'Blue Dog',nE:'Experiment',pNe:'8675309990',eE:'edit0@gmail.com',tE:'t19'},
+]
